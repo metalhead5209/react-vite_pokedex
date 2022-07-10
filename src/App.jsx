388dd9d1271c7bedc37f2318a2f2a0b10721523d
Aaron Rodi <metalhead5209@gmail.com>
2022-07-10 11:58:53 -0500

    added pokecard component with data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-
+import React, { Component } from 'react';
+import Pokecard from './components/Pokecard';
 import './App.css'
 
-function App() {
-  
 
-  return (
-    <div className="App">
-      
-    </div>
-  )
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pokecard id={6} name="Charazard" type="fire" exp={98} />
+      </div>
+   ) 
+  }
 }
 
-export default App
+
+export default App;
